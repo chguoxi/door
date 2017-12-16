c25d72b95a5ff91bffe175e97f21288a98389cbe
chguoxi <995306681@qq.com>
2017-12-16 22:58:09 +0800

    展示标题

--- application/core/MY_Controller.php
+++ application/core/MY_Controller.php
@@ -2,9 +2,16 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class MY_Controller extends CI_Controller {
+    protected $title = '罗马大道';
+    protected $discription = '条条大路通罗马，这条最宽敞';
+    protected $keywords = '';
     
     public function display($tpl,$data=array(),$ret=false){
-        $this->load->view('common/header');
+        $data['title'] = $this->title;
+        $data['discription'] = $this->discription;
+        $data['keywords'] = $this->keywords;
+        
+        $this->load->view('common/header',$data);
         $this->load->view($tpl,$data,$ret);
         $this->load->view('common/header');
     }
